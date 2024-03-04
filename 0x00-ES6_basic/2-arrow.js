872725821c82cb1a65a7c7c9f35387c72c7a912d
pha12thom <geofreymilugo@gmail.com>
2024-03-04 21:37:01 +0000

    arrow js

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,10 @@
 export default function NeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  // Using an arrow function to preserve the 'this' context
-  this.addNeighborhood = newNeighborhood => {
-    this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return this.sanFranciscoNeighborhoods;
+  const self = this
+this.addNeighborhood = newNeighborhood => {
+	this.sanFranciscoNeighborhoods.push(newNeighborhood);
+	return this.sanFranciscoNeighborhoods;
   };
 }
 
